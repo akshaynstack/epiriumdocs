05477d8592acb46b209379274864b13c229ad38c
AKSHAY N <94807520+akshaynstack@users.noreply.github.com>
2023-07-25 01:52:23 +0530

    beta-release

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,32 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  banner: {
+    key: '1.0-release',
+    text: (
+      <a href="#" target="_blank">
+        🎉 Epirium 1.0 is released. BETA →
+      </a>
+    )
+  },
+  logo: <span>Epirium Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/akshaynstack/epiriumdocs',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://twitter.com/akshaynceo',
+    icon: (
+      <svg width="24" height="24" viewBox="0 0 256 256">
+        <path
+          fill="currentColor"
+          d="m231.9 169.8l-94.8 65.6a15.7 15.7 0 0 1-18.2 0l-94.8-65.6a16.1 16.1 0 0 1-6.4-17.3L45 50a12 12 0 0 1 22.9-1.1L88.5 104h79l20.6-55.1A12 12 0 0 1 211 50l27.3 102.5a16.1 16.1 0 0 1-6.4 17.3Z"
+        ></path>
+      </svg>
+    )
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/akshaynstack/epiriumdocs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Epirium © MIT 2023',
   },
 }
 
